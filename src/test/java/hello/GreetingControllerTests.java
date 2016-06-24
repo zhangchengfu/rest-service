e4f158e3673848e1d649764f4616f63433f23a8c
zhangchengfu <compowerxp@163.com>
2016-06-24 21:16:56 +0800

    rest service

---
+++ src/test/java/hello/GreetingControllerTests.java
@@ -0,0 +1,53 @@
+package hello;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class GreetingControllerTests {
+	
+	@Autowired
+	private WebApplicationContext ctx;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/greeting"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").value("Hello, World"));
+	}
+	
+	@Test
+	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("name", "Spring Community"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.content").value("Hello, Spring Community"));
+				
+	}
+}
